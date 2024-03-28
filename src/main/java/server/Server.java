0707552bca37723c9a5fbb28353b42265189ee77
jowjow22 <jonathan224santos@gmail.com>
2024-03-27 21:05:09 -0300

    feat: initial configuration to socket

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,7 +1,47 @@
 package server;
 
-public class Server {
-    public void uppercaseMessage(String message){
-        System.out.println("Server "+message.toUpperCase());
+import java.io.*;
+import java.net.*;
+
+public class Server extends Thread{
+    private static final Socket client;
+    public static void main(String[] args) throws IOException  {
+        InetAddress ip = InetAddress.getByName("127.0.0.1");
+        try(ServerSocket server = new ServerSocket(10007, 0, ip);){
+            while(true){
+                try {
+                    System.out.println("Waiting for Connection");
+                    server.accept();
+                } catch (IOException e) {
+                    System.err.println("Accept failed.");
+                    System.exit(1);
+                }
+            }
+        }
+        catch (IOException e){
+            System.err.println(e);
+        }
+    }
+    @Override
+    public void run(){
+        System.out.println("New thread started");
+
+        try(
+                client;
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        ){
+            String inputLine;
+            while((inputLine = in.readLine())!= null){
+                System.out.println("Message from" + client.getInetAddress() + ": "+ inputLine);
+
+            }
+            out.close();
+            in.close();
+            client.close();
+        }
+        catch (IOException e){
+
+        }
     }
 }
